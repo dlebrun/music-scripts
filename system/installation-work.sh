9c3e8642195feae5f088d16ed5cbe65274b79433
Dominique Lebrun <dominique.lebrun@belighted.com>
2020-02-07 12:05:06 +0100

    Separate home installation script from work installation script

---
+++ system/installation-work.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# Create group "belighted" and set it as default
+sudo addgroup --gid 1001 belighted
+sudo adduser dle belighted
+sudo usermod -g belighted dle
+sudo delgroup dle
+
+# Install extra softwares
+sudo add-apt-repository ppa:kubuntu-ppa/backports
+sudo add-apt-repository ppa:serge-rider/dbeaver-ce
+sudo apt update
+sudo apt full-upgrade
+sudo apt install build-essential git curl docker.io awscli flac kubuntu-restricted-addons kubuntu-restricted-extras network-manager-openvpn software-properties-common dbeaver-ce qgit
+sudo snap install rubymine --classic
+sudo snap install slack --classic
+sudo snap install bitwarden
+sudo snap install google-cloud-sdk --classic
+sudo snap install kubectl --classic
+
+sudo adduser dle docker
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://raw.githubusercontent.com/docker/compose/1.25.0/contrib/completion/bash/docker-compose" -o /etc/bash_completion.d/docker-compose
+
+base=https://github.com/docker/machine/releases/download/v0.16.0 &&
+  curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
+  sudo mv /tmp/docker-machine /usr/local/bin/docker-machine &&
+  chmod +x /usr/local/bin/docker-machine
+
+base=https://raw.githubusercontent.com/docker/machine/v0.16.0
+for i in docker-machine-prompt.bash docker-machine-wrapper.bash docker-machine.bash
+do
+  sudo wget "$base/contrib/completion/bash/${i}" -P /etc/bash_completion.d
+done
+
+echo 'fs.inotify.max_user_watches = 524288' | sudo tee -a /etc/sysctl.d/idea.conf > /dev/null
+sudo sysctl -p --system
