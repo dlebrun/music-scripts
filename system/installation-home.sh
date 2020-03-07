6c2e874b7cf91c762ea5dc853239cf6ef10eb5e3
Dominique Lebrun <dom.lochet@gmail.com>
2020-03-07 16:00:43 +0100

    Add user to dialout group + refacto

--- system/installation-home.sh
+++ system/installation-home.sh
@@ -5,6 +5,7 @@ set -e
 # Create group "sda" and set it as default
 sudo addgroup --gid 1001 sda
 sudo adduser dom sda
+sudo adduser dom dialout
 sudo usermod -g sda dom
 sudo delgroup dom
 
@@ -15,13 +16,10 @@ sudo sh -c "echo \"UUID=32eeea79-f205-48dd-ab0f-8e21c05ef741 /mnt/backups    ext
 
 # Install extra softwares
 sudo add-apt-repository ppa:kubuntu-ppa/backports
+sudo add-apt-repository ppa:musicbrainz-developers/stable
+sudo add-apt-repository ppa:team-xbmc/ppa
+sudo add-apt-repository ppa:subsurface/subsurface
 sudo apt update
 sudo apt full-upgrade
-sudo apt install git flac kubuntu-restricted-extras network-manager-openvpn ffmpeg software-properties-common libdvd-pkg
+sudo apt install git flac kubuntu-restricted-extras network-manager-openvpn ffmpeg software-properties-common libdvd-pkg kodi picard
 sudo dpkg-reconfigure libdvd-pkg
-sudo add-apt-repository ppa:team-xbmc/ppa
-sudo apt update
-sudo apt install kodi
-sudo add-apt-repository ppa:musicbrainz-developers/stable
-sudo apt update
-sudo apt install picard
