c63270c3c62817f06acb6fa9ace621f05b6e44f6
Dominique Lebrun <dom.lochet@gmail.com>
2020-08-29 10:01:37 +0200

    Add missing softwares in home installation script

--- system/installation-home.sh
+++ system/installation-home.sh
@@ -20,5 +20,7 @@ sudo add-apt-repository ppa:team-xbmc/ppa
 sudo add-apt-repository ppa:subsurface/subsurface
 sudo pkcon refresh
 sudo pkcon update
-sudo apt install git flac kubuntu-restricted-extras network-manager-openvpn ffmpeg software-properties-common libdvd-pkg kodi picard subsurface muon kate
+sudo apt install git flac kubuntu-restricted-extras network-manager-openvpn ffmpeg software-properties-common libdvd-pkg kodi picard subsurface muon kate k3b digikam ktorrent
 sudo dpkg-reconfigure libdvd-pkg
+sudo snap install bitwarden
+sudo snap install authy
